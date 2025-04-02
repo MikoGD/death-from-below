ec70ead46eeb6cb26a69c9438e2be3f20e328c96
Mikael Escolin <mikaelescolin@gmail.com>
2025-04-02 23:08:27 +0100

    Fix formatting

--- main.go
+++ main.go
@@ -11,121 +11,121 @@ import (
 	"strings"
 )
 
-type InvalidArgsError struct {}
-type InvalidSourceArg struct {}
-type InvalidGlobArg struct {}
+type InvalidArgsError struct{}
+type InvalidSourceArg struct{}
+type InvalidGlobArg struct{}
 
 func (e InvalidArgsError) Error() string {
-  return "Invalid amount of arguments"
+	return "Invalid amount of arguments"
 }
 
 func (e InvalidSourceArg) Error() string {
-  return "Invalid source arg"
+	return "Invalid source arg"
 }
 
 func (e InvalidGlobArg) Error() string {
-  return "Invalid glob arg"
+	return "Invalid glob arg"
 }
 
 func searchAndReplace() error {
 	if len(os.Args) != 5 {
-    return InvalidArgsError{}
+		return InvalidArgsError{}
 	}
 
 	sourceDir := os.Args[1]
-  globPattern := os.Args[2]
-  stringToReplaceFrom := os.Args[3]
-  stringToReplaceTo := os.Args[4]
+	globPattern := os.Args[2]
+	stringToReplaceFrom := os.Args[3]
+	stringToReplaceTo := os.Args[4]
 
-  if sourceDir == "" {
-    return InvalidSourceArg{}
-  }
+	if sourceDir == "" {
+		return InvalidSourceArg{}
+	}
 
-  if globPattern == "" {
-    return InvalidGlobArg{}
-  }
+	if globPattern == "" {
+		return InvalidGlobArg{}
+	}
 
-  err := filepath.WalkDir(sourceDir, func(path string, dir os.DirEntry, err error) error {
-    if dir.IsDir() {
-      return nil
-    }
+	err := filepath.WalkDir(sourceDir, func(path string, dir os.DirEntry, err error) error {
+		if dir.IsDir() {
+			return nil
+		}
 
 		if err != nil {
 			return err
 		}
 
-    isMatch, err := filepath.Match(globPattern, filepath.Base(path))
+		isMatch, err := filepath.Match(globPattern, filepath.Base(path))
 
-    if err != nil {
-      return err
-    }
+		if err != nil {
+			return err
+		}
 
-    if !isMatch {
-      return nil
-    }
+		if !isMatch {
+			return nil
+		}
 
 		file, err := os.Open(path)
 		if err != nil {
 			return err
 		}
 
-    defer file.Close()
+		defer file.Close()
 
-    ext := filepath.Ext(path)
+		ext := filepath.Ext(path)
 
-    modifiedFileName := fmt.Sprintf("%s-modified%s", file.Name(), ext)
-    modifiedFile, err := os.Create(modifiedFileName)
+		modifiedFileName := fmt.Sprintf("%s-modified%s", file.Name(), ext)
+		modifiedFile, err := os.Create(modifiedFileName)
 		reader := bufio.NewReader(file)
 		for {
-      line, err := reader.ReadString('\n')
-      if errors.Is(err, io.EOF) {
-        lineToWrite := line
-        if strings.Contains(line, stringToReplaceFrom) {
-          lineToWrite = strings.Replace(line, stringToReplaceFrom, stringToReplaceTo, 1)
-        }
+			line, err := reader.ReadString('\n')
+			if errors.Is(err, io.EOF) {
+				lineToWrite := line
+				if strings.Contains(line, stringToReplaceFrom) {
+					lineToWrite = strings.Replace(line, stringToReplaceFrom, stringToReplaceTo, 1)
+				}
 
-        if _, err := modifiedFile.WriteString(lineToWrite); err != nil {
-          return err
-        }
+				if _, err := modifiedFile.WriteString(lineToWrite); err != nil {
+					return err
+				}
 
-        break
-      }
+				break
+			}
 
-      if err != nil {
-        removeErr := os.RemoveAll(modifiedFileName)
-        return errors.New(removeErr.Error() + "\n" + err.Error())
-      }
+			if err != nil {
+				removeErr := os.RemoveAll(modifiedFileName)
+				return errors.New(removeErr.Error() + "\n" + err.Error())
+			}
 
-      lineToWrite := line
-      if strings.Contains(line, stringToReplaceFrom) {
-        lineToWrite = strings.ReplaceAll(line, stringToReplaceFrom, stringToReplaceTo)
-      }
+			lineToWrite := line
+			if strings.Contains(line, stringToReplaceFrom) {
+				lineToWrite = strings.ReplaceAll(line, stringToReplaceFrom, stringToReplaceTo)
+			}
 
-      if _, err = modifiedFile.WriteString(lineToWrite); err != nil {
-        return err
-      }
+			if _, err = modifiedFile.WriteString(lineToWrite); err != nil {
+				return err
+			}
 		}
 
-    if err := os.Remove(file.Name()); err != nil {
-      return err
-    }
+		if err := os.Remove(file.Name()); err != nil {
+			return err
+		}
 
-    if err:= os.Rename(modifiedFile.Name(), file.Name()); err != nil {
-      return err
-    }
+		if err := os.Rename(modifiedFile.Name(), file.Name()); err != nil {
+			return err
+		}
 
 		return nil
 	})
 
-  if err != nil {
-    return  err
-  }
+	if err != nil {
+		return err
+	}
 
-  return nil 
+	return nil
 }
 
 func main() {
-  if err := searchAndReplace(); err != nil {
-    log.Fatalf("%v\n", err)
-  }
+	if err := searchAndReplace(); err != nil {
+		log.Fatalf("%v\n", err)
+	}
 }
